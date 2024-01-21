0002275b50f04a25f975eb8d5ebac8b25b84ff74
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-01-21 19:02:12 +0530

    commit

--- instagram_manager/urls.py
+++ instagram_manager/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [
-    path('link-instagram/<int:pk>/', create_instagram_user_access)
-]
+urlpatterns = [path("link-instagram/<int:pk>/", create_instagram_user_access)]
