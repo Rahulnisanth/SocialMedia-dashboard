bda24954d82facae25aad8268db097c4d00df9db
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-01-21 15:39:23 +0530

    added-requiredModels

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(
+        null=True,
+        blank=True,
+        default="static/assets/img/profile-cover.jpg",
+        upload_to="media/profiles/",
+    )
+    firstname = models.CharField(max_length=200, null=True, blank=True)
+    lastname = models.CharField(max_length=200, null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    email = models.CharField(null=True, blank=True, max_length=150, unique=True)
+    short_intro = models.CharField(null=True, blank=True, max_length=1000)
+    # ADDRESS MODULES...
+    address = models.CharField(max_length=200, null=True, blank=True)
+    number = models.BigIntegerField(default=0, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    state = models.CharField(max_length=200, null=True, blank=True)
+    code = models.IntegerField(default=000000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True, primary_key=True
+    )
+
+    def __str__(self):
+        return str(self.user)
--- instagram_manager/models.py
+++ instagram_manager/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
+class Instagram(models.Model):
+    user_id = models.CharField(max_length=350, blank=True, null=True)
+    access_token = models.CharField(max_length=350, blank=True, null=True)
+    password = models.CharField(max_length=350, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True, primary_key=True
+    )
+
+    def __str__(self):
+        return str(self.user_id)
+
+
+class Post(models.Model):
+    user_id = models.ForeignKey(
+        Instagram, null=True, blank=True, on_delete=models.CASCADE
+    )
+    post_name = models.CharField(max_length=350, blank=True, null=True)
+    comments = models.IntegerField(default=0, null=True, blank=True)
+    likes = models.IntegerField(default=0, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True, primary_key=True
+    )
+
+    def __str__(self):
+        return str(self.post_name)
+
+
+class Account(models.Model):
+    user_id = models.ForeignKey(
+        Instagram, null=True, blank=True, on_delete=models.CASCADE
+    )
+    followers = models.IntegerField(default=0, null=True, blank=True)
+    followings = models.IntegerField(default=0, null=True, blank=True)
+    media_count = models.IntegerField(default=0, null=True, blank=True)
+    profile_url = models.CharField(max_length=250, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True, primary_key=True
+    )
+
+    def __str__(self):
+        return str(self.user_id)
