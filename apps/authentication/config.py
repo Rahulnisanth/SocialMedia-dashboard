73cec47c640e019258298326e78bff991107f183
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-01-21 18:21:35 +0530

    added-profileFinals

--- apps/authentication/config.py
+++ apps/authentication/config.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class AuthConfig(AppConfig):
     name = "apps.authentication"
     label = "apps_authentication"
-
-    # NEEDED FOR SIGNALS MODULE :
-    def ready(self):
-        from . import signals
